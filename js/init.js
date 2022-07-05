ba60fae06aeca8e68ab6bf8d53b16c07f4a0ef7f
Denys <daseev@icloud.com>
2022-07-05 20:40:35 +0300

    finish

--- js/init.js
+++ js/init.js
@@ -6,4 +6,8 @@ $( function() {
       prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',      
       nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>' 
     });
+    $('.header-nav').slicknav({
+      appendTo: '.header .container-fluid',
+      label: ''
+    });
   } );
